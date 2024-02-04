43a47c32fa7d38f861d01e5bebe60139bd7dd87f
Sora Park <welcometosorapark@gmail.com>
2024-02-04 15:26:23 +0900

    add initial files (basic structure)

---
+++ routes/postings.js
@@ -0,0 +1,8 @@
+/**
+ * The routes for posting management (write, edit)
+ */
+
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+/**
+ * The routes for user management (login, signup, info, update)
+ */
+
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
